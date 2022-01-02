b2bf297027719a858433b0b39bb35832ea5ceaba
Артём Витальевич Шульга <75699163+OopsWoopsssss@users.noreply.github.com>
2022-01-02 23:15:39 +0300

    Жоска

---
+++ frontend-shop/src/App.vue
@@ -0,0 +1,59 @@
+<template>
+  <div id="app">
+    <div class="container">
+      <span>
+        {{ test }}
+      </span>
+      <br>
+      <button v-on:click="loadProduct">
+        ++
+      </button>
+    </div>
+  </div>
+</template>
+
+<script>
+
+import axios from 'axios'
+export default {
+  // state: {
+  //       backendUrl: "http://127.0.0.1:8000/api/v1"
+  //   },
+  name: 'App',
+  data() {
+    return {
+      test: 1,
+      backendUrl: "http://127.0.0.1:8000/api/v1/product",
+      listProduct: [],
+    }
+  },
+  methods: {
+    incor() {
+      this.test++
+    },
+    getServerUrl() {
+      return this.backendUrl
+    },
+    async loadProduct() {
+      let apaaaa = await axios.get('http://127.0.0.1:8000/api/v1/product/')
+      console.log(apaaaa)
+      // this.listProduct = await fetch('http://127.0.0.1:8000/api/v1/product/')
+      //     .then(response => response.json())
+      //     .then(data => console.log(data[0].name))
+    },
+  },
+
+  components: {}
+}
+</script>
+
+<style>
+.container {
+  max-width: 350px;
+  margin: 0 auto;
+}
+
+.w-100 {
+  width: 100%;
+}
+</style>
