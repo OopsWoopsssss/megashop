8aeb90e6af786b727e486b7472c08153755f7318
Max <39224167+Fenzdy@users.noreply.github.com>
2022-01-04 23:25:18 +0300

    +

--- shop-frontend/src/main.js
+++ shop-frontend/src/main.js
@@ -1,12 +1,18 @@
 import Vue from 'vue'
 import App from './App.vue'
-import router from './router'
-import store from './store'
+import VueRouter from 'vue-router'
+
+import router from './routher'
+import 'bootstrap'
+import 'bootstrap/dist/css/bootstrap.min.css'
+
+Vue.use(VueRouter);
 
 Vue.config.productionTip = false
 
-new Vue({
+const app = new Vue({
+  render: h => h(App),
   router,
-  store,
-  render: h => h(App)
-}).$mount('#app')
+})
+
+app.$mount('#app')
