0e48a174bf761740ce730c361cc1ef466541bb98
Max <39224167+Fenzdy@users.noreply.github.com>
2022-01-09 18:27:40 +0300

    +

--- shop-frontend/src/components/cards/cards.vue
+++ shop-frontend/src/components/cards/cards.vue
@@ -1,13 +1,13 @@
 <template>
   <div class="col-12 col-sm-6 col-md-4 col-lg-3" v-for="product in products" v-bind:key="product.id">
     <div class="card">
-      <img class="card-img-top" v-bind:src="product.poster" alt="Card image cap">
+      <img class="card-img-top" v-bind:src="'http://127.0.0.1:8000' + product.poster" alt="Card image cap">
       <div class="card-body">
         <h4 class="card-title">
           {{ product.title }}
         </h4>
-        <router-link v-bind:to="product.category">
-          <b-badge variant="secondary">{{ product.category }}</b-badge>
+        <router-link v-for="category in product.category" v-bind:key="category" v-bind:to="category">
+          <b-badge class="me-2 d-inline-block" variant="secondary">{{ category }}</b-badge>
         </router-link>
         <p class="mt-3">{{ product.price }} р.</p>
         <div class="d-flex justify-content-between">
