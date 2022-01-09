5a53455cbd45b04fdbdb4e738fac301d832e5a17
Артём Витальевич Шульга <75699163+OopsWoopsssss@users.noreply.github.com>
2022-01-09 19:42:17 +0300

    drf_yasg

--- shop_backend/shop_backend/urls.py
+++ shop_backend/shop_backend/urls.py
@@ -17,6 +17,7 @@ from django.conf import settings
 from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import path, include
+from .yasg import urlpatterns as doc_urls
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -24,6 +25,7 @@ urlpatterns = [
     path('api/v1/', include('main.urls')),
 ]
 
+urlpatterns += doc_urls
 
 
 if settings.DEBUG:
